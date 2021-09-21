d3da16a6360a03dc2493a178b52bfed42fb38f8a
Kerry Wang <zwang34@email.wm.edu>
2021-09-21 19:09:37 -0400

    Resolving code style issues, formatting

--- AnalogControl.c
+++ AnalogControl.c
@@ -9,7 +9,7 @@
 //Final Voltage NUmeric control		CTRL_PANEL_NUMFINALVAL
 // Units indicator					CTRL_PANEL_STRUNITS
 //Timescale	numeric control			CTRL_PANEL_NUMTIMESCALE
-//Command button to set values		CTRL_PANEL_CMD_SETANALOG 
+//Command button to set values		CTRL_PANEL_CMD_SETANALOG
 
 /*
 This panel is used to set the value of an analog control.  It is called by double-clicking on an
@@ -18,78 +18,43 @@ Clicking on Set Value, reads the values from the control and stores them in the 
 
 */
 
-
-
 //***********************************************************************
-int CVICALLBACK CMD_SETANALAOG_CALLBACK (int panel, int control, int event,
-		void *callbackData, int eventData1, int eventData2)
-/* Panel button to set analog data. 
+int CVICALLBACK CMD_SETANALAOG_CALLBACK(int panel, int control, int event,
+										void *callbackData, int eventData1, int eventData2)
+/* Panel button to set analog data.
 Reads data off of the panel controls, ringbox and numberboxes.
 Writes data to the AnalogTable array
 NOT DONE:  IF TIMESCALE IS TOO LONG THEN OVERWRITE THE FOLLOWING CELL
 */
 {
-	int itemp=0,cmode;				// variables for reading/writing the control mode
-	int docheck=0,valuesgood=1;		// booleans.  docheck=1 if we need to check the time settings
-									// valuesgood=1 if okay to write data to AnalogTable array..sets to 0 if prompt denied
-	double dtemp,ttemp,usedtimescale; //usedtimescale=timescale written in AnalogTable to control ramping, exponential etc.
-	double timematrixlength;		// duration of the current column.
+	int itemp = 0; // variables for reading/writing the control mode
+	double dtemp, ttemp;
 	switch (event)
+	{
+
+	case EVENT_COMMIT:
+		//Retrieve Control/Data Values from Panel
+		GetCtrlVal(panelHandle4, CTRL_PANEL_RING_CTRLMODE, &itemp);
+		GetCtrlVal(panelHandle4, CTRL_PANEL_NUMFINALVAL, &dtemp);
+		GetCtrlVal(panelHandle4, CTRL_PANEL_NUMTIMESCALE, &ttemp);
+		AnalogTable[currentx][currenty][currentpage].fcn = itemp;
+
+		if (itemp != 6)
 		{
-								 
-		case EVENT_COMMIT:
-			valuesgood=1;
-			
-			//Retrieve Control/Data Values from Panel
-			GetCtrlVal (panelHandle4,CTRL_PANEL_RING_CTRLMODE, &itemp);
-			cmode=itemp;
-			GetCtrlVal (panelHandle4,CTRL_PANEL_NUMFINALVAL, &dtemp);
-			GetCtrlVal (panelHandle4,CTRL_PANEL_NUMTIMESCALE, &ttemp);
-			usedtimescale=dtemp;
-			AnalogTable[currentx][currenty][currentpage].fcn=itemp;
-			
-			if(itemp!=6)
-			{
-				AnalogTable[currentx][currenty][currentpage].fval=dtemp;			
-				AnalogTable[currentx][currenty][currentpage].tscale=ttemp;
-			}
-		
-			GetCtrlVal (panelHandle4,CTRL_PANEL_NUMFINALVAL, &dtemp);
-			AnalogTable[currentx][currenty][currentpage].fval=dtemp;			
-			
-			GetCtrlVal (panelHandle4,CTRL_PANEL_NUMTIMESCALE, &dtemp);
-			usedtimescale=dtemp;
-			AnalogTable[currentx][currenty][currentpage].tscale=dtemp;
-			
-			// Check if the timescale may be too long, if so prompt for 
-			//overwrite of the next block.
-			timematrixlength=TimeArray[currentx][currentpage];
-			docheck=0;
-			if((cmode==1)&&(usedtimescale>timematrixlength)) {docheck=1;}
-			if((cmode==2)&&(usedtimescale>timematrixlength)) {docheck=1;}
-			if((cmode==3)&&(usedtimescale>timematrixlength/3)) {docheck=1;}
-			if(cmode==0) {docheck=0;}
-			
-			// Code below used to allow ramps to have shorter time constants... basically disabled now
-			if (docheck==1)
-			{
-				//valuesgood=ConfirmPopup("Overlong array selected","Do you want to overwrite the following array?");	
-				valuesgood=1;  // just assume timing is right.
-				if(valuesgood==1)
-				{
-				 //code to update the 2 columns
-				
-				}
-			}
-			if(valuesgood==1)  // everything checks out, return to main panel 
-			{
-				HidePanel(panelHandle4);
-				DrawNewTable(1);
-				break;
-			}
-			
-			
+			AnalogTable[currentx][currenty][currentpage].fval = dtemp;
+			AnalogTable[currentx][currenty][currentpage].tscale = ttemp;
 		}
+
+		GetCtrlVal(panelHandle4, CTRL_PANEL_NUMFINALVAL, &dtemp);
+		AnalogTable[currentx][currenty][currentpage].fval = dtemp;
+
+		GetCtrlVal(panelHandle4, CTRL_PANEL_NUMTIMESCALE, &dtemp);
+		AnalogTable[currentx][currenty][currentpage].tscale = dtemp;
+
+		HidePanel(panelHandle4);
+		DrawNewTable(1);
+		break;
+	}
 	return 0;
 }
 //***************************************************************************
@@ -97,128 +62,134 @@ void SetControlPanel(void)
 {
 	// Reads the value of the last active column before this one and displays it on the panel.
 	double lastvalue;
-	BOOL FOUNDVAL=0;
-	int cx,cz;
-	
-	SetCtrlVal(panelHandle4,CTRL_PANEL_STRUNITS, AChName[currenty].units);
-	SetCtrlVal(panelHandle4,CTRL_PANEL_STR_CHNAME,AChName[currenty].chname);
-	SetCtrlVal (panelHandle4,CTRL_PANEL_NUMFINALVAL, AnalogTable[currentx][currenty][currentpage].fval);
-	SetCtrlVal (panelHandle4,CTRL_PANEL_NUMTIMESCALE, AnalogTable[currentx][currenty][currentpage].tscale);
-	SetCtrlVal (panelHandle4,CTRL_PANEL_RING_CTRLMODE,AnalogTable[currentx][currenty][currentpage].fcn);
+	BOOL FOUNDVAL = 0;
+	int cx, cz;
+
+	SetCtrlVal(panelHandle4, CTRL_PANEL_STRUNITS, AChName[currenty].units);
+	SetCtrlVal(panelHandle4, CTRL_PANEL_STR_CHNAME, AChName[currenty].chname);
+	SetCtrlVal(panelHandle4, CTRL_PANEL_NUMFINALVAL, AnalogTable[currentx][currenty][currentpage].fval);
+	SetCtrlVal(panelHandle4, CTRL_PANEL_NUMTIMESCALE, AnalogTable[currentx][currenty][currentpage].tscale);
+	SetCtrlVal(panelHandle4, CTRL_PANEL_RING_CTRLMODE, AnalogTable[currentx][currenty][currentpage].fcn);
 
 	// find the last value
-	cx=1;
-	cz=1;
-	lastvalue=0;
-	while(!FOUNDVAL)
+	cx = 1;
+	cz = 1;
+	lastvalue = 0;
+	while (!FOUNDVAL)
 	{
-		if(TimeArray[cx][cz]==0) {cz++;cx=1;}       // if you see a 0 time, go to next page
-		if(cz>=NUMBEROFPAGES)
+		if (cz >= NUMBEROFPAGES) // technically NUMBEROFPAGES is 1 more than the actual number of pages, inconsistent I know -KW
 		{
-			FOUNDVAL=TRUE;
+			break; // didn't find it, end the loop
 		}
-		if(TimeArray[cx][cz]<0) 					// if you see a negative time, skip to next value
+		if (TimeArray[cx][cz] == 0)
 		{
-			cx++;  
-			if(cx>=NUMBEROFCOLUMNS) {cx=1;cz++;}
-		}										   
-		if(TimeArray[cx][cz]>0)					    // if positive time, record current val     
-		{	
-			
-			if((cx>=currentx)&&(cz>=currentpage)) 
-			{	
-				FOUNDVAL=TRUE;	   // found current value
+			cz++; // if you see a 0 time, go to next page
+			cx = 1;
+		}
+		else if (TimeArray[cx][cz] < 0) // if you see a negative time, skip to next value
+		{
+			cx++;
+			if (cx >= NUMBEROFCOLUMNS)
+			{
+				cx = 1;
+				cz++;
+			}
+		}
+		else // if positive time, record current val
+		{
+			if ((cx >= currentx) && (cz >= currentpage))
+			{
+				FOUNDVAL = TRUE; // found current value
 			}
 			else
 			{
-				lastvalue=AnalogTable[cx][currenty][cz].fval;
+				lastvalue = AnalogTable[cx][currenty][cz].fval;
 			}
 			cx++;
-			if(cx>=NUMBEROFCOLUMNS) {cx=1;cz++;}
+			if (cx >= NUMBEROFCOLUMNS)
+			{
+				cx = 1;
+				cz++;
+			}
 		}
 	}
-	
-	SetCtrlVal(panelHandle4,CTRL_PANEL_NUMINITVAL,lastvalue);
+
+	SetCtrlVal(panelHandle4, CTRL_PANEL_NUMINITVAL, lastvalue);
 }
 
 //***************************************************************************
 
-int CVICALLBACK RING_CTRLMODE_CALLBACK (int panel, int control, int event,
-		void *callbackData, int eventData1, int eventData2)
+int CVICALLBACK RING_CTRLMODE_CALLBACK(int panel, int control, int event,
+									   void *callbackData, int eventData1, int eventData2)
 /* activated when using the control mode ring control.
 Cycles between the different control modes and changes the timescale of the ramp
-to an appropriate value for the mode. 
-If adding another function, just add another case, and modify the ring control on the 
+to an appropriate value for the mode.
+If adding another function, just add another case, and modify the ring control on the
 AnalogControl.uir file, and add function handling in CalcFcnVal() in GUIDesign.c
 
 */
 {
-	int ctrlmode=0;
-	double timescales=0,newtime=1,Vdiff=0,ctrlvfinal;
-	
-	GetCtrlVal (panelHandle4,CTRL_PANEL_RING_CTRLMODE, &ctrlmode);
-	GetCtrlVal (panelHandle4,CTRL_PANEL_NUMFINALVAL, &ctrlvfinal);
-	timescales=TimeArray[currentx][currentpage];
-	Vdiff=ctrlvfinal-AnalogTable[currentx-1][currenty][currentpage].fval;
-	SetCtrlAttribute (panelHandle4, CTRL_PANEL_NUMFINALVAL, ATTR_LABEL_TEXT,"Final Value");
-	SetCtrlAttribute (panelHandle4, CTRL_PANEL_NUMTIMESCALE, ATTR_LABEL_TEXT,"Time Scale");
+	int ctrlmode = 0;
+	double timescales = 0, Vdiff = 0, ctrlvfinal;
+
+	GetCtrlVal(panelHandle4, CTRL_PANEL_RING_CTRLMODE, &ctrlmode);
+	GetCtrlVal(panelHandle4, CTRL_PANEL_NUMFINALVAL, &ctrlvfinal);
+	timescales = TimeArray[currentx][currentpage];
+	Vdiff = ctrlvfinal - AnalogTable[currentx - 1][currenty][currentpage].fval;
+	SetCtrlAttribute(panelHandle4, CTRL_PANEL_NUMFINALVAL, ATTR_LABEL_TEXT, "Final Value");
+	SetCtrlAttribute(panelHandle4, CTRL_PANEL_NUMTIMESCALE, ATTR_LABEL_TEXT, "Time Scale");
 	switch (event)
-		{
-		case EVENT_LEFT_CLICK:
-			break;
-		case EVENT_LEFT_DOUBLE_CLICK:
-	
-			break;
-			case EVENT_LOST_FOCUS:
-			
-		
-			case EVENT_COMMIT:
-			break;
-		
-		}
+	{
+	case EVENT_LEFT_CLICK:
+		break;
+	case EVENT_LEFT_DOUBLE_CLICK:
+
+		break;
+	case EVENT_LOST_FOCUS:
+
+	case EVENT_COMMIT:
+		break;
+	}
 	switch (ctrlmode)
 	{
-		case 1://stepwise
-			SetCtrlVal (panelHandle4,CTRL_PANEL_NUMTIMESCALE, 0.0);
-			break;
-		case 2://linear ramp
-			SetCtrlVal (panelHandle4,CTRL_PANEL_NUMTIMESCALE, timescales);
-			break;
-		case 3://exponential
-			SetCtrlVal (panelHandle4,CTRL_PANEL_NUMTIMESCALE, timescales);
-			if(!(Vdiff==0))
-			{
-				newtime=timescales/fabs(log(fabs(Vdiff))-log(0.001));
+	case 1: //stepwise
+		SetCtrlVal(panelHandle4, CTRL_PANEL_NUMTIMESCALE, 0.0);
+		break;
+	case 2: //linear ramp
+		SetCtrlVal(panelHandle4, CTRL_PANEL_NUMTIMESCALE, timescales);
+		break;
+	case 3: //exponential
+		SetCtrlVal(panelHandle4, CTRL_PANEL_NUMTIMESCALE, timescales);
+		if (!(Vdiff == 0))
+		{
+			double newtime = timescales / fabs(log(fabs(Vdiff)) - log(0.001));
 			//	newtime=fabs(timescales/(log(0.001/Vdiff)));
-				SetCtrlVal (panelHandle4,CTRL_PANEL_NUMTIMESCALE, newtime);
-			}
-			break;//const. jerk
-		case 4:
-			SetCtrlVal (panelHandle4,CTRL_PANEL_NUMTIMESCALE,timescales);
-			break;
-			
-		case 5: //S
-			SetCtrlAttribute (panelHandle4, CTRL_PANEL_NUMFINALVAL, ATTR_LABEL_TEXT,"Amplitude");
-			SetCtrlAttribute (panelHandle4, CTRL_PANEL_NUMTIMESCALE, ATTR_LABEL_TEXT,"Frequency");
-			break;
-	
+			SetCtrlVal(panelHandle4, CTRL_PANEL_NUMTIMESCALE, newtime);
+		}
+		break; //const. jerk
+	case 4:
+		SetCtrlVal(panelHandle4, CTRL_PANEL_NUMTIMESCALE, timescales);
+		break;
+
+	case 5: //S
+		SetCtrlAttribute(panelHandle4, CTRL_PANEL_NUMFINALVAL, ATTR_LABEL_TEXT, "Amplitude");
+		SetCtrlAttribute(panelHandle4, CTRL_PANEL_NUMTIMESCALE, ATTR_LABEL_TEXT, "Frequency");
+		break;
 	}
-			
+
 	return 0;
 }
 //***************************************************************************
 
-int CVICALLBACK CMD_ANCANCEL_CALLBACK (int panel, int control, int event,
-		void *callbackData, int eventData1, int eventData2)
-// Cancel and don't make any changes.  
+int CVICALLBACK CMD_ANCANCEL_CALLBACK(int panel, int control, int event,
+									  void *callbackData, int eventData1, int eventData2)
+// Cancel and don't make any changes.
 {
 	switch (event)
-		{
-		case EVENT_COMMIT:
-			 HidePanel(panelHandle4);
-			break;
-		}
+	{
+	case EVENT_COMMIT:
+		HidePanel(panelHandle4);
+		break;
+	}
 	return 0;
 }
-
-
--- DigitalSettings.c
+++ DigitalSettings.c
@@ -3,95 +3,95 @@
 #include "DigitalSettings.h"
 #include "DigitalSettings2.h"
 /*
-Configure the digital lines.  
-For each line we configure 
- 			chnum: 		channel number (1-32 on first card, 101-132 on second card) 
-            chname:     name of the channel.             
-			resettolow:	Define 
+Configure the digital lines.
+For each line we configure
+			chnum: 		channel number (1-32 on first card, 101-132 on second card)
+			chname:     name of the channel.
+			resettolow:	Define
 
 */
 
 //********************************************************************************************
-int CVICALLBACK NUM_DIG_LINE_CALLBACK (int panel, int control, int event,
-		void *callbackData, int eventData1, int eventData2)
-/*  
+int CVICALLBACK NUM_DIG_LINE_CALLBACK(int panel, int control, int event,
+									  void *callbackData, int eventData1, int eventData2)
+/*
 Numeric control to update information displayed on the digital settings panel, based on the current line
 */
 
 {
-	int line=0;
+	int line = 0;
 	switch (event)
-		{
-		case EVENT_COMMIT:
-			GetCtrlVal (panelHandle3, DIGPANEL_NUM_DIGCH_LINE, &line);
-			SetCtrlVal (panelHandle3, DIGPANEL_NUM_DIGCHANNEL, DChName[line].chnum);
-			SetCtrlVal (panelHandle3, DIGPANEL_STR_DIGCHANNELNAME, DChName[line].chname);
-			SetCtrlVal (panelHandle3, DIGPANEL_CHK_DIGRESET, DChName[line].resettolow);			
-			break;
-		}
+	{
+	case EVENT_COMMIT:
+		GetCtrlVal(panelHandle3, DIGPANEL_NUM_DIGCH_LINE, &line);
+		SetCtrlVal(panelHandle3, DIGPANEL_NUM_DIGCHANNEL, DChName[line].chnum);
+		SetCtrlVal(panelHandle3, DIGPANEL_STR_DIGCHANNELNAME, DChName[line].chname);
+		SetCtrlVal(panelHandle3, DIGPANEL_CHK_DIGRESET, DChName[line].resettolow);
+		break;
+	}
 	return 0;
 }
 //********************************************************************************************
-int CVICALLBACK CMD_DIGALLOWCHANGE_CALLBACK (int panel, int control, int event,
-		void *callbackData, int eventData1, int eventData2)
-/*  
+int CVICALLBACK CMD_DIGALLOWCHANGE_CALLBACK(int panel, int control, int event,
+											void *callbackData, int eventData1, int eventData2)
+/*
 Button to allow changes to be made to textboxes.
 Changes textbox from dimmed (indicator) to activate (hot) mode
 */
 {
-	SetCtrlAttribute (panelHandle3, DIGPANEL_CMD_DIGSETCHANGES,   ATTR_VISIBLE, 1);
+	SetCtrlAttribute(panelHandle3, DIGPANEL_CMD_DIGSETCHANGES, ATTR_VISIBLE, 1);
 	switch (event)
-		{
-		case EVENT_COMMIT:
-			
-			SetCtrlAttribute (panelHandle3, DIGPANEL_NUM_DIGCHANNEL, ATTR_CTRL_MODE, VAL_HOT);
-			SetCtrlAttribute (panelHandle3, DIGPANEL_STR_DIGCHANNELNAME, ATTR_CTRL_MODE, VAL_HOT);
-			SetCtrlAttribute (panelHandle3, DIGPANEL_CHK_DIGRESET, ATTR_CTRL_MODE, VAL_HOT);			
-			break;
-		}
+	{
+	case EVENT_COMMIT:
+
+		SetCtrlAttribute(panelHandle3, DIGPANEL_NUM_DIGCHANNEL, ATTR_CTRL_MODE, VAL_HOT);
+		SetCtrlAttribute(panelHandle3, DIGPANEL_STR_DIGCHANNELNAME, ATTR_CTRL_MODE, VAL_HOT);
+		SetCtrlAttribute(panelHandle3, DIGPANEL_CHK_DIGRESET, ATTR_CTRL_MODE, VAL_HOT);
+		break;
+	}
 	return 0;
 }
 //********************************************************************************************
-int CVICALLBACK CMD_DIGSETCHANGES_CALLBACK (int panel, int control, int event,
-		void *callbackData, int eventData1, int eventData2)
+int CVICALLBACK CMD_DIGSETCHANGES_CALLBACK(int panel, int control, int event,
+										   void *callbackData, int eventData1, int eventData2)
 /*
 Set the digital line information DChName, based on information in text boxes.
 */
-{	
-	int channel=0,line=0,resetlow=0;
+{
+	int channel = 0, line = 0, resetlow = 0;
 	char buff[50];
 	switch (event)
-		{
-		case EVENT_COMMIT:
-			GetCtrlVal (panelHandle3, DIGPANEL_NUM_DIGCHANNEL, &channel);
-			GetCtrlVal (panelHandle3, DIGPANEL_NUM_DIGCH_LINE, &line);
-			GetCtrlVal (panelHandle3, DIGPANEL_STR_DIGCHANNELNAME, buff);
-			GetCtrlVal (panelHandle3, DIGPANEL_CHK_DIGRESET, &resetlow);
-			DChName[line].chnum=channel;
-			sprintf(DChName[line].chname,buff);
-			DChName[line].resettolow=resetlow;
-			SetDigitalChannels();
-			break;
-		}
+	{
+	case EVENT_COMMIT:
+		GetCtrlVal(panelHandle3, DIGPANEL_NUM_DIGCHANNEL, &channel);
+		GetCtrlVal(panelHandle3, DIGPANEL_NUM_DIGCH_LINE, &line);
+		GetCtrlVal(panelHandle3, DIGPANEL_STR_DIGCHANNELNAME, buff);
+		GetCtrlVal(panelHandle3, DIGPANEL_CHK_DIGRESET, &resetlow);
+		DChName[line].chnum = channel;
+		sprintf(DChName[line].chname, buff);
+		DChName[line].resettolow = resetlow;
+		SetDigitalChannels();
+		break;
+	}
 	return 0;
 }
 //********************************************************************************************
-int CVICALLBACK CMD_DONEDIG_CALLBACK (int panel, int control, int event,
-		void *callbackData, int eventData1, int eventData2)
+int CVICALLBACK CMD_DONEDIG_CALLBACK(int panel, int control, int event,
+									 void *callbackData, int eventData1, int eventData2)
 /*
 Turn control boxes into inactive (indicator) mode and hide the panel
 */
 {
 	switch (event)
-		{
-		case EVENT_COMMIT:
-			SetCtrlAttribute (panelHandle3, DIGPANEL_NUM_DIGCHANNEL, ATTR_CTRL_MODE, VAL_INDICATOR);
-			SetCtrlAttribute (panelHandle3, DIGPANEL_STR_DIGCHANNELNAME, ATTR_CTRL_MODE, VAL_INDICATOR);
-			SetCtrlAttribute (panelHandle3, DIGPANEL_CMD_DIGSETCHANGES,   ATTR_VISIBLE, 0);
-			SetCtrlAttribute (panelHandle3, DIGPANEL_CHK_DIGRESET,   ATTR_CTRL_MODE, VAL_INDICATOR);
-			HidePanel(panelHandle3);
-			break;
-		}
+	{
+	case EVENT_COMMIT:
+		SetCtrlAttribute(panelHandle3, DIGPANEL_NUM_DIGCHANNEL, ATTR_CTRL_MODE, VAL_INDICATOR);
+		SetCtrlAttribute(panelHandle3, DIGPANEL_STR_DIGCHANNELNAME, ATTR_CTRL_MODE, VAL_INDICATOR);
+		SetCtrlAttribute(panelHandle3, DIGPANEL_CMD_DIGSETCHANGES, ATTR_VISIBLE, 0);
+		SetCtrlAttribute(panelHandle3, DIGPANEL_CHK_DIGRESET, ATTR_CTRL_MODE, VAL_INDICATOR);
+		HidePanel(panelHandle3);
+		break;
+	}
 	return 0;
 }
 
@@ -101,25 +101,24 @@ void SetDigitalChannels(void)
 Set the digital channel list (on main panel) with the information contained in the DChName array
 */
 {
-	int i=0,j=0,k=0,line=0;
-	char numbuff[20]="";
-	for(i=1;i<=NUMBERDIGITALCHANNELS;i++)
+	int i = 0;
+	for (i = 1; i <= NUMBERDIGITALCHANNELS; i++)
 	{
-		SetTableCellVal (panelHandle, PANEL_TBL_DIGNAMES, MakePoint(1,i),DChName[i].chname);
-		SetTableCellVal (panelHandle, PANEL_TBL_DIGNAMES, MakePoint(2,i),DChName[i].chnum);
+		SetTableCellVal(panelHandle, PANEL_TBL_DIGNAMES, MakePoint(1, i), DChName[i].chname);
+		SetTableCellVal(panelHandle, PANEL_TBL_DIGNAMES, MakePoint(2, i), DChName[i].chnum);
 	}
 }
 
 //********************************************************************************************
 
-int CVICALLBACK CHKDIGRESET_CALLBACK (int panel, int control, int event,
-		void *callbackData, int eventData1, int eventData2)
+int CVICALLBACK CHKDIGRESET_CALLBACK(int panel, int control, int event,
+									 void *callbackData, int eventData1, int eventData2)
 {
 	switch (event)
-		{
-		case EVENT_COMMIT:
+	{
+	case EVENT_COMMIT:
 
-			break;
-		}
+		break;
+	}
 	return 0;
 }
